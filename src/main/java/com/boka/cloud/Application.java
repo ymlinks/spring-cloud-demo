c3a70940290a60d6ae425f7889d505480113a26f
Ymlinks-PC <fuzhaohui200@gmail.com>
2018-05-19 13:40:42 +0800

    1. 升级spring boot 到2.0.2.RELEASE 2. 更新相应的依赖库

--- src/main/java/com/boka/cloud/Application.java
+++ src/main/java/com/boka/cloud/Application.java
@@ -1,11 +1,21 @@
 package com.boka.cloud;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@EnableWebMvc
+@EnableCaching
+@ComponentScan("com.boka")
+@MapperScan("com.boka.cloud.mapper")
+@EnableMongoRepositories("com.boka.*.repository")
 public class Application {
     public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+        SpringApplication.run(Application.class, args);
+    }
 }
---
+++ src/main/java/com/boka/cloud/config/DBConfig.java
@@ -0,0 +1,84 @@
+package com.boka.cloud.config;
+
+import com.alibaba.druid.filter.Filter;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.wall.WallConfig;
+import com.alibaba.druid.wall.WallFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("ALL")
+@Configuration
+@EnableTransactionManagement
+public class DBConfig {
+
+    @Bean
+    public PlatformTransactionManager transactionManager() throws SQLException {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        List<Filter> filterList = new ArrayList<>();
+        filterList.add(wallFilter());
+        druidDataSource.setProxyFilters(filterList);
+        druidDataSource.setInitialSize(30);
+        druidDataSource.setMaxActive(100);
+        druidDataSource.setMinIdle(30);
+        druidDataSource.setTestOnBorrow(false);
+        druidDataSource.setTestOnReturn(false);
+        druidDataSource.setTestWhileIdle(true);
+        druidDataSource.setMaxWait(60000);
+        druidDataSource.setValidationQuery("select 1");
+        druidDataSource.setMinEvictableIdleTimeMillis(252000000);
+        druidDataSource.setTimeBetweenEvictionRunsMillis(60000);
+        druidDataSource.setRemoveAbandoned(true);
+        druidDataSource.setRemoveAbandonedTimeout(1800);
+        return druidDataSource;
+    }
+
+    @Bean
+    public WallFilter wallFilter() {
+        WallFilter wallFilter = new WallFilter();
+        wallFilter.setConfig(wallConfig());
+        return wallFilter;
+    }
+
+    @Bean
+    public WallConfig wallConfig() {
+        WallConfig config = new WallConfig();
+        config.setMultiStatementAllow(true);//允许一次执行多条语句
+        config.setNoneBaseStatementAllow(true);//允许非基本语句的其他语句
+        return config;
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDbFactory mongoDbFactory,
+                                       MongoMappingContext context) {
+
+        MappingMongoConverter converter = new MappingMongoConverter(new DefaultDbRefResolver(mongoDbFactory), context);
+        converter.afterPropertiesSet();
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null)); // 去掉_class字段
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory, converter);
+        return mongoTemplate;
+
+    }
+
+}
