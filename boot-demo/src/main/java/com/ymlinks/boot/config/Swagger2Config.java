480eb09672b8c929bfd51fdd7971684b33a2a2b6
fuzhaohui <fuzhaohui200@gmail.com>
2018-06-26 14:36:36 +0800

    1. Reactor Code

--- boot-demo/src/main/java/com/ymlinks/boot/config/Swagger2Config.java
+++ boot-demo/src/main/java/com/ymlinks/boot/config/Swagger2Config.java
@@ -20,7 +20,7 @@ public class Swagger2Config {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.ymlinks.cloud.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.ymlinks.boot.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
