d824d3ea96372dc67f8aadd90fb9ad553fc9a894
fuzhaohui <fuzhaohui200@gmail.com>
2018-06-28 10:10:25 +0800

    1. Reactor Code 修改过期类

---
+++ boot-demo/src/main/java/com/ymlinks/boot/config/MvcConfig.java
@@ -0,0 +1,49 @@
+package com.ymlinks.boot.config;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.ymlinks.boot.interceptor.AuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("ALL")
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthInterceptor authInterceptor;
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        List<MediaType> mediaTypes = new ArrayList<>();
+        mediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fastConverter.setSupportedMediaTypes(mediaTypes);
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setCharset(Charset.forName("UTF-8"));
+        fastJsonConfig.setSerializerFeatures(
+                SerializerFeature.PrettyFormat,
+                SerializerFeature.DisableCircularReferenceDetect,
+                SerializerFeature.IgnoreNonFieldGetter,
+                SerializerFeature.BrowserSecure,
+                SerializerFeature.IgnoreErrorGetter
+        );
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        converters.add(fastConverter);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor);
+    }
+}
