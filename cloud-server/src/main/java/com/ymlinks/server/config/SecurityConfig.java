dbe0671a9e05d0cbfd68415deb7f38455d636fb5
Fu Zhaohui <fuzhaohui200@gmail.com>
2018-09-11 22:20:20 +0800

    1. Reactor Code

---
+++ cloud-server/src/main/java/com/ymlinks/server/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.ymlinks.server.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@Order(1)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("test")
+                .password("T123456").roles("SYSTEM");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                .and().requestMatchers().antMatchers("/eureka/**")
+                .and().authorizeRequests().antMatchers("/eureka/**")
+                .hasRole("SYSTEM").anyRequest().denyAll().and()
+                .httpBasic().and().csrf().disable();
+    }
+}
