39d564d742402f87c4950c3e780d5e22e568160c
Fu Zhaohui <fuzhaohui200@gmail.com>
2018-09-18 20:55:31 +0800

    1. Reactor Code

--- cloud-demo/src/main/java/com/ymlinks/cloud/CloudApplication.java
+++ cloud-demo/src/main/java/com/ymlinks/cloud/CloudApplication.java
@@ -1,12 +1,20 @@
 package com.ymlinks.cloud;
 
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.xpack.client.PreBuiltXPackTransportClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Slf4j
 @EnableDiscoveryClient
@@ -17,5 +25,15 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class CloudApplication {
     public static void main(String[] args) {
         SpringApplication.run(CloudApplication.class, args);
+    }
+
+    @Bean
+    public TransportClient transportClient() throws UnknownHostException {
+        return new PreBuiltXPackTransportClient(Settings.builder()
+                .put("cluster.name", "docker-cluster")
+                .put("client.transport.ignore_cluster_name", false)
+                .put("xpack.security.user", "elastic:123456")
+                .build())
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("192.168.141.160"), 9300));
     }
 }
