19e66f8052e3316b98ef893ed7fb362f927d1abe
符朝辉 <fuzhaohui@beadwallet.com>
2018-11-08 16:07:36 +0800

    1. Reactor Code

--- cloud-demo/src/main/java/com/ymlinks/cloud/CloudApplication.java
+++ cloud-demo/src/main/java/com/ymlinks/cloud/CloudApplication.java
@@ -1,20 +1,12 @@
 package com.ymlinks.cloud;
 
 import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.xpack.client.PreBuiltXPackTransportClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 @Slf4j
 @EnableDiscoveryClient
--- config-server/src/main/java/com/ymlinks/config/ConfigApplication.java
+++ config-server/src/main/java/com/ymlinks/config/ConfigApplication.java
@@ -2,10 +2,13 @@ package com.ymlinks.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringCloudApplication
 public class ConfigApplication {
     
     public static void main(String[] args) {
