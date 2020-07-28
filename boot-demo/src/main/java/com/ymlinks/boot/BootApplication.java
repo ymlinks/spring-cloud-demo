c1b985310347c3d75b14a9b3c4aec2b8ad3360aa
Fu Zhaohui <fuzhaohui200@gmail.com>
2020-07-29 06:22:12 +0800

    1. 优化插件

--- boot-demo/src/main/java/com/ymlinks/boot/BootApplication.java
+++ boot-demo/src/main/java/com/ymlinks/boot/BootApplication.java
@@ -3,16 +3,12 @@ package com.ymlinks.boot;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Slf4j
-@EnableDiscoveryClient
 @EnableWebMvc
 @EnableCaching
 @SpringBootApplication
--- boot-demo/src/main/java/com/ymlinks/boot/dto/MbDto.java
+++
@@ -1,9 +0,0 @@
-package com.ymlinks.boot.dto;
-
-import lombok.Data;
-
-@Data
-public class MbDto {
-    private Integer a;
-    private Byte b;
-}
